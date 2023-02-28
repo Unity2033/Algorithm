72f126681f55da41ad50c9b8958d65ea389cb589
mol1986 <82032086+Unity2033@users.noreply.github.com>
2023-02-28 19:50:45 +0900

    (2022.2.28) 이진 트리

--- DataStruct.cpp
+++ DataStruct.cpp
@@ -2,34 +2,60 @@
 
 using namespace std;
 
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* CreateNode(int data, Node* leftChild, Node* rightChild)
+{
+    // 노드 객체를 생성해야 합니다.
+    Node * newNode = new Node;
+
+    newNode->data = data;
+
+    newNode->left = leftChild;
+
+    newNode->right = rightChild;
+
+    return newNode;
+}
+
+void Preorder(Node * root)
+{
+    if (root != NULL)
+    {
+        // 1. 자기 자신의 데이터를 출력합니다.
+        cout << root->data << endl;
+
+        // 2. 왼쪽 노드 방문
+        Preorder(root->left);
+
+        // 3. 오른쪽 노드 방문
+        Preorder(root->right);
+    }
+}
 
 int main()
 {
-    // 삽입 정렬
-    // 배열의 모든 요소를 앞에서부터 차례대로 이미 정렬된
-    // 배열과 비교하여, 자신의 위치를 찾아 삽입하는 정렬입니다.
-  
-    int array[5] = { 5, 3, 7, 1, 2 };
-    int key = 0;
-    int j = 0;
+    // 트리 구조란?
+    // 한 노드에서 시작해서 다른 정점들을 순회하여
+    // 자기 자신에게 돌아오는 순환이 없는 연결 그래프입니다.
 
-    for (int i = 1; i < 5; i++)
-    {
-        key = array[i];
+    // 이진 트리란?
+    // 최대 2개의 자식 노드를 가지는 트리입니다.
+    Node* node7 = CreateNode(70, NULL, NULL);
+    Node* node6 = CreateNode(60, NULL, NULL);
+    Node* node5 = CreateNode(50, NULL, NULL);
+    Node* node4 = CreateNode(40, NULL, NULL);
 
-        for (j = i - 1; j >= 0 && array[j] > key; j--)
-        {
-            array[j + 1] = array[j];
-        }
-
-        array[j + 1] = key;
-    }
-
-    for (const int & element : array)
-    {
-        cout << element << endl;
-    }
-
+    Node* node3 = CreateNode(30, node6, node7);
+    Node* node2 = CreateNode(20, node4, node5);
+    Node* node1 = CreateNode(10, node2, node3);
+                        
+    Preorder(node1);
 
     return 0;
 }
