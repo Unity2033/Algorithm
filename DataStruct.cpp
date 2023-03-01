459df9258233dd4eb0924eeba188b53a7e91fb45
mol1986 <82032086+Unity2033@users.noreply.github.com>
2023-03-01 19:48:52 +0900

    (2023.3.1) 계수 정렬

--- DataStruct.cpp
+++ DataStruct.cpp
@@ -1,61 +1,42 @@
 #include <iostream>
 
+#define SIZE 10
 using namespace std;
-
-struct Node
-{
-    int data;
-    Node* left;
-    Node* right;
-};
-
-Node* CreateNode(int data, Node* leftChild, Node* rightChild)
-{
-    // 노드 객체를 생성해야 합니다.
-    Node * newNode = new Node;
-
-    newNode->data = data;
-
-    newNode->left = leftChild;
-
-    newNode->right = rightChild;
-
-    return newNode;
-}
-
-void Preorder(Node * root)
-{
-    if (root != NULL)
-    {
-        // 1. 자기 자신의 데이터를 출력합니다.
-        cout << root->data << endl;
-
-        // 2. 왼쪽 노드 방문
-        Preorder(root->left);
-
-        // 3. 오른쪽 노드 방문
-        Preorder(root->right);
-    }
-}
 
 int main()
 {
-    // 트리 구조란?
-    // 한 노드에서 시작해서 다른 정점들을 순회하여
-    // 자기 자신에게 돌아오는 순환이 없는 연결 그래프입니다.
+    // 계수 정렬
+    /*
+    // 데이터의 값을 직접 비교하지 않고, 단순하게 각 숫자가
+    // 몇 개 있는지 갯수를 세어 저장한 다음 정렬하는 알고리즘입니다.
 
-    // 이진 트리란?
-    // 최대 2개의 자식 노드를 가지는 트리입니다.
-    Node* node7 = CreateNode(70, NULL, NULL);
-    Node* node6 = CreateNode(60, NULL, NULL);
-    Node* node5 = CreateNode(50, NULL, NULL);
-    Node* node4 = CreateNode(40, NULL, NULL);
+    int array[5] = { 0, };
+    int item[SIZE] = { 1, 5, 3, 2, 1, 2, 2, 3, 4, 4 };
 
-    Node* node3 = CreateNode(30, node6, node7);
-    Node* node2 = CreateNode(20, node4, node5);
-    Node* node1 = CreateNode(10, node2, node3);
-                        
-    Preorder(node1);
+    for (int i = 0; i < SIZE; i++)
+    {
+        array[item[i] - 1] += 1;
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << array[i] << endl;
+    }
+    */
+
+    // 대문자로 입력한 문자를 소문자로 바꾸는 알고리즘
+    string content;
+    cin >> content;
+
+    for (int i = 0; i < content.length(); i++)
+    {
+        if (content[i] >= 'A' && content[i] <= 'Z')
+        {
+            content[i] += 32;
+        }
+    }
+
+    cout << content << endl;
 
     return 0;
 }
