4da04aa2a7dc7fd00ec336d5d65fc7381a9ca044
mol1986 <82032086+Unity2033@users.noreply.github.com>
2023-03-10 19:37:48 +0900

    (2023.3.10) 병합 정렬

--- DataStruct.cpp
+++ DataStruct.cpp
@@ -2,9 +2,73 @@
 
 using namespace std;
 
+int arr[8] = { 5, 3, 1, 2, 4, 7, 6, 8 };
+
+void MergeSort(int data [], int start, int middle, int end)
+{
+    // left 변수, right 변수
+    // 정렬 시킬 배열의 인덱스를 증가시키는 변수 (temp)
+    int left = start;
+    int right = middle + 1;
+    int temp = start;
+
+    while (left <= middle && right <= end)
+    {
+        if (data[left] <= data[right])
+        {
+            arr[temp++] = data[left++];
+        }
+        else // data[left] > data[right]
+        {
+            arr[temp++] = data[right++];
+        }
+    }
+
+    // 남아 있는 배열 순서대로 넣어주기
+    if (left > middle)
+    {
+        for (int x = right; x <= end; x++)
+        {
+            arr[temp++] = data[x];
+        }
+    }
+    else
+    {
+        for (int x = left; x <= middle; x++)
+        {
+            arr[temp++] = data[x];
+        }
+    }
+
+    // 정렬된 배열에 데이터를 삽입니다.
+    for (int i = start; i <= end; i++)
+    {
+        data[i] = arr[i];
+    }                
+}
+
+void Division(int data [], int left, int right)
+{
+    if (left < right)
+    {
+        // middle 변수 선언
+        int middle = (left + right) / 2;
+
+        // 왼쪽 분할 재귀 함수
+        Division(data, left, middle);
+
+        // 오른쪽 분할 재귀 함수
+        Division(data, middle + 1, right);
+
+        // 병합    
+        MergeSort(data, left, middle, right);
+    }
+}
+
 int main()
 {
     // 문자열 뒤집기
+    /*
     string content;
 
     cin >> content;
@@ -15,6 +79,20 @@ int main()
     }                
 
     cout << content << endl;
+    */
+
+    // 병합 정렬
+    // 하나의 리스트를 두 개의 균등한 크기로 분할하고
+    // 분할된 부분 리스트를 정렬한 다음, 두 개의 정렬된
+    // 부분 리스트를 합하여 전체가 정렬된 리스트가 되도록 
+    // 정렬하는 알고리즘입니다.
+    Division(arr, 0, 7);
+
+    for (int i = 0; i < 8; i++)
+    {
+        cout << arr[i] << endl;
+    }
+
 
     return 0;
 }
---
+++ QuickSort/QuickSort.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+using namespace std;
+
+
+int arr[10] = { 4,1,2,3,9,7,8,6,10,5 };
+
+void QuickSort(int data[], int start, int end)
+{
+    // 원소가 1개 있을 경우
+    if (start >= end)
+    {
+        return;
+    }
+
+    // left, right, pivot 변수
+    int pivot = start;
+    int left = pivot + 1;
+    int right = end;
+
+    while (left <= right)
+    {
+        // left가 end보다 작거나 같다면 반복
+        // 왼쪽에 있는 left 변수를 pivot과 비교합니다.
+        while (left <= end && arr[left] < arr[pivot])
+        {
+            left++;
+        }
+
+        // right가 start보다 크다면
+        // 오른쪽에 있는 right 변수를 pivot과 비교합니다.
+        while (right > start && arr[right] > arr[pivot])
+        {
+            right--;
+        }
+
+        // if 왼쪽과 오른쪽이 엇갈렸을 때
+        if (left > right)
+        {
+            swap(arr[pivot], arr[right]);
+        }
+        // else right와 left의 값을 교환합니다.
+        else
+        {
+            swap(arr[right], arr[left]);
+        }
+    }
+
+    // 분할 정복
+    QuickSort(data, start, right - 1);
+    QuickSort(data, right + 1, end);
+}
+
+int main()
+{
+    // 기점이 되는 키를 기준으로 작거나 같은 값을
+    // 지닌 데이터는 앞으로 이동하고 큰 값을 지닌
+    // 데이터는 뒤로 이동합니다.
+
+    // 그리고 작은 값과 큰 값을 분리하면서 정렬하는 알고리즘입니다.
+
+    QuickSort(arr, 0, 9);
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << arr[i] << endl;
+    }
+
+    return 0;
+}
