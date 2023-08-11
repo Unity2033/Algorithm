e202f704c58726c22d3ff36cee12d0cb92bd171e
Unity2033 <kileab@naver.com>
2023-08-11 19:44:34 +0900

    (2023.8.11) 랜덤 함수

--- Application/Application.cpp
+++ Application/Application.cpp
@@ -1,118 +1,83 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
-#pragma region 함수 포인터  
-     // 함수의 주소값을 저장하고 가리킬 수 있는 변수입니다.
+int life;
+int computer;
+int answer;
 
-void Delegate()
+void Init()
 {
-	printf("대리자~");
+	srand(time(NULL));
+
+	life = 5;
+	computer = rand() % 50 + 1;
 }
 
-int Add(int x, int y)
+void Update()
 {
-	return x + y;
+	printf("컴퓨터가 가지고 있는 값 : ");
+	scanf_s("%d", &answer);
 }
 
-int Substract(int x, int y)
+void Render()
 {
-	return x - y;
+	if (computer == answer)
+	{
+		printf("\n게임에서 승리하였습니다.\n");
+		exit(1);
+	}
+	else if (computer > answer)
+	{
+		printf("\n컴퓨터가 가지고 있는 값보다 작습니다.\n");
+		life--;
+	}
+	else if (computer < answer)
+	{
+		printf("\n컴퓨터가 가지고 있는 값보다 큽니다.\n");
+		life--;
+	}
+
+	if (life <= 0)
+	{
+		printf("\n게임에서 패배하였습니다.\n");
+	}
 }
 
-int Multiple(int x, int y)
-{
-	return x * y;
-}
-
-int Divide(int x, int y)
-{
-	return x / y;
-}
-
-void Calculator(int x, int y, int (*cptr) (int, int))
-{
-	printf("cptr 함수의 결과 : %d\n", cptr(x, y));
-}
-#pragma endregion
 
 
 int main()
 {
-#pragma region 함수 포인터
+#pragma region 랜덤 함수
+	// 0 ~ 32767 사이의 난수 값을 반환하는 함수입니다.
 
-	// 함수 포인터는 함수의 반환형과 매개 변수의 타입이 일치해야 하며,
-	// 함수 포인터를 사용하여 동적으로 메모리를 할당할 수 없습니다.
-	// void (*fptr) ();
-	// 
-	// Calculator(1, 1, Add);
-	// Calculator(20, 10, Substract);
-	// Calculator(5, 5, Multiple);
-	// Calculator(20, 20, Divide);
-	// 
-	// fptr = Delegate;
-	// 
-	// fptr();
-
+	//	int seed = 0;
+	//	
+	//	// 1970년 1월 1일 ~
+	//	srand(time(NULL));
+	//	
+	//	for (int i = 0; i < 10; i++)
+	//	{ 
+	//		seed = rand() % 10 + 1;
+	//		printf("seed의 값 : %d \n", seed);
+	//	}
 #pragma endregion
 
-#pragma region 소수 판별 알고리즘
+#pragma region UP-DOWN 게임
 
-	//	int number = 0;
-	//	int count = 0;
-	//	
-	//	scanf_s("%d", &number);
-	//	
-	//	for (int i = 2; i <= number; i++)
-	//	{
-	//		if (number % i == 0)
-	//		{
-	//			count++;
-	//		}
-	//	}
-	//	
-	//	if (count == 1)
-	//	{
-	//		printf("소수입니다.\n");
-	//	}
-	//	else
-	//	{
-	//		printf("소수가 아닙니다.\n");
-	//	}
+	// 게임 데이터 초기화
+	Init();
 
-#pragma endregion
-
-#pragma region 회문 판별 알고리즘
-
-	int flag = 0;
-
-	const char * content = "asdda";
-	
-	int size = strlen(content);
-
-	// [l][e]   [v]   [e][l]
-	for (int i = 0; i < size / 2; i++)
+	while (life > 0)
 	{
-		if (content[i] == content[size - 1 - i])
-		{
-			flag = 1;
-		}
-		else
-		{
-			flag = 0;
-			break;
-		}
+		// 게임 업데이트 함수
+		Update();
+
+		// 게임 렌더링 함수
+		Render();
 	}
 
-	if (flag == 1)
-	{
-		printf("회문입니다.");
-	}
-	else
-	{
-		printf("회문이 아닙니다.");
-	}
 
-		
 #pragma endregion
 
 
