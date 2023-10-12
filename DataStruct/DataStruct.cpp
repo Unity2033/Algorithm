d85d33890924f942bac8429d273c89b306d32aea
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2023-10-12 19:47:26 +0900

    (2023.10.12) 양방향 연결 리스트

--- DataStruct/DataStruct.cpp
+++ DataStruct/DataStruct.cpp
@@ -1,51 +1,7 @@
-#include <iostream>
-
-using namespace std;
-
-#pragma region 단일 연결 리스트
-
-
-#pragma endregion
-
-
-
-void PushFront(Node * target, int data)
-{
-    Node * newNode = new Node;
-    newNode->data = data;
-
-    newNode->next = target->next;
-
-    target->next = newNode;
-}
-
-void Remove(Node * target)
-{
-    Node * deleteNode = target->next;
-
-    target->next = deleteNode->next;
-
-    delete deleteNode;
-}
-
+#include "SingleLinkedList.h"
 
 int main()
 {
-    Node * head = new Node;
-    head->next = nullptr;
-
-    PushFront(head, 10);
-    PushFront(head, 20);
-
-    Remove(head);
-
-    Node * currentPtr = head->next;
-
-    while (currentPtr != nullptr)
-    {
-        cout << currentPtr->data << endl;
-        currentPtr = currentPtr->next;
-    }
 
 }
 
