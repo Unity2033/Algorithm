841cf3e0325b0f4c09069461068fd5b5678be345
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2023-11-01 19:48:28 +0900

    (2023.11.1) 이진 탐색 트리 삭제

--- DataStruct/DataStruct.cpp
+++ DataStruct/DataStruct.cpp
@@ -1,13 +1,17 @@
-#include <iostream>
 #include "BinarySearchTree.h"
-
-using namespace std;
 
 int main()
 {
-
 	BinarySearchTree<int> bst;
-	bst.Insert(1, bst.GetRoot());
+
+	bst.rootNode = bst.Insert(10, bst.rootNode);
+
+	bst.Insert(5, bst.rootNode);
+	bst.Insert(15, bst.rootNode);
+
+	cout << bst.FindMax(bst.rootNode) << endl;
+
+	bst.Inorder(bst.GetRoot());
 
 	return 0;
 }
