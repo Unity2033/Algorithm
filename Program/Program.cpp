5fa22d6e864a076a02cf42e92059ccdfa88f00f8
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2024-04-26 21:55:31 +0900

    (2024.4.26) 다익스트라

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,107 +1,122 @@
 ﻿#include <iostream>
-#include <vector>
-#include <queue>
+#include <limits.h>
+
+#define SIZE 5
+#define INFINITY 10000000
 
 using namespace std;
-
-#define SIZE 8
 
 class Graph
 {
 private:
-	int inDegree[SIZE];
-	std::vector<int> edge[SIZE];
-
-public:
-	Graph()
+	int weight[SIZE][SIZE] =
 	{
-		for (int i = 0; i < SIZE; i++)
+		{0       ,7,4       ,6       ,1 }   ,
+		{INFINITY,0,INFINITY,INFINITY,INFINITY },
+		{INFINITY,2,0,5              ,INFINITY }   ,
+		{INFINITY,3,INFINITY,0       ,INFINITY }   ,
+		{INFINITY,INFINITY,    INFINITY,  1,0 }   ,
+	};
+
+	bool visited[SIZE];
+	int distance[SIZE];
+
+	public:
+		Graph()
 		{
-			inDegree[i] = 0;
-		}
-	}
-
-	void ConnectEdge(int x, int y)
-	{
-		edge[x].push_back(y);
-
-		inDegree[y]++;
-	}
-
-	void Sort()
-	{
-		std::queue<int> queue;
-
-		for (int i = 1; i < SIZE; i++)
-		{
-			if (inDegree[i] == 0)
+			for (int i = 0; i < SIZE; i++)
 			{
-				queue.push(i);
+				visited[i] = 0;
 			}
 		}
 
-		for (int i = 1; i < SIZE; i++)
+		// 최소 비용 노드를 탐색하는 함수
+		int SmallNode()
 		{
-			if (queue.empty())
+			int min = INFINITY;
+			int minPosition = 0;
+
+			for (int i = 0; i < SIZE; i++)
 			{
-				break;
-			}
-
-			int x = queue.front();
-
-			queue.pop();
-
-			cout << x << " ";
-
-			for (int j = 0; j < edge[x].size(); j++)
-			{
-				int y = edge[x][j];
-
-				inDegree[y]--;
-
-				if (inDegree[y] <= 0)
+				if (distance[i] < min && visited[i] == false)
 				{
-					queue.push(y);
+					min = distance[i];
+					minPosition = i;
 				}
 			}
+
+			return minPosition;
 		}
-	}
+
+		void Dijkstra(int start)
+		{
+			for (int i = 0; i < SIZE; i++)
+			{
+				distance[i] = weight[start][i];
+			}
+
+			visited[start] = true;
+
+			for (int i = 0; i < SIZE - 1; i++)
+			{
+				int minNode = SmallNode(); // 최소 비용 노드 탐색
+
+				visited[minNode] = true;
+
+				for (int j = 0; j < SIZE; j++)
+				{
+					// 방문하지 않은 노드 중에서 시작점이 최소 비용 노드를 
+					// 경유하는게 더 가까우면 값을 갱신합니다.
+					if (visited[j] == false)
+					{
+						if (distance[minNode] + weight[minNode][j] < distance[j])
+						{
+							distance[j] = distance[minNode] + weight[minNode][j];
+						}
+					}
+				}
+
+			}
+		}
+
+		void Distance()
+		{
+			for (int i = 0; i < SIZE; i++)
+			{
+				cout << distance[i] << " ";
+			}
+		}
 };
 
 int main()
 {
-#pragma region 위상 정렬
-	// 방향 그래프에 존재하는 각 정점들의 선행 순서를 지키며
-	// 모든 정점을 차례대로 진행하는 알고리즘입니다.
+#pragma region 다익스트라 알고리즘
+	// 시작점으로부터 모든 노드까지의 최소 거리를 구해주는
+	// 알고리즘입니다.
 
-	// 사이클이 발생하는 경우 위상 정렬을 수행할 수 없습니다.
-	
-	// DAG(Directed Acyclic Graph) : 사이클이 존재하지 않는 그래프
-	// 시간 복잡도 O (V + E) 
-	
-	// 위상 정렬
-	// 1. 진입 차수가 0인 정점을 Queue에 삽입합니다.
+	// 1. distance 배열의 weight[시작점 노드]의 값들로
+	//    초기화시켜줍니다.
 
-	// 2. Queue에서 원소를 꺼내 연결된 모든 간선을 제거합니다.
+	// 2. 시작점을 방문 처리합니다.
+	 
+	// 3. distance 배열에서 최소 비용 노드를 찾고 방문 처리합니다.
+	//    단, 이미 방문한 노드는 제외합니다.
 	
-	// 3. 간선 제거 이후에 진입 차수가 0이 된 정점을 Queue에 삽입합니다.
-	
-	// 4. Queue가 비어있을 때까지 2번 ~ 3번을 반복 수행합니다.  
-	
+	// 4. 최소 비용 노드를 거쳐갈지 고려해서 distance 배열을 갱신합니다.
+	//    단, 이미 방문한 노드는 제외합니다.
+
+    // 5. 모든 노드를 방문할 때까지 3번 ~ 4번을 반복합니다.
+
+	// 방문하지 않은 노드 중에서 가장 작은 distance를 가진 노드를
+	// 방문하고, 그 노드와 연결된 다른 노드까지의 거리를 계산합니다.
+
 	Graph graph;
 
-	graph.ConnectEdge(1, 2);
-	graph.ConnectEdge(1, 5);
-	graph.ConnectEdge(2, 3);
-	graph.ConnectEdge(3, 4);
-	graph.ConnectEdge(3, 5);
-	graph.ConnectEdge(4, 6);
-	graph.ConnectEdge(5, 6);
-	graph.ConnectEdge(6, 7);
+	graph.Dijkstra(0);
 
-	graph.Sort();
+	graph.Distance();
 
-#pragma endregion
+#pragma endregion								  
 
 
 
